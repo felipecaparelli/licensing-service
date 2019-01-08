a4551d43e0e97917a8393d6270adf3e7c1c37277
Felipe Caparelli <felipecaparelli@gmail.com>
2019-01-08 16:45:05 +0100

    Using resttemplate injected by Spring

--- src/main/java/com/wro/licenses/Application.java
+++ src/main/java/com/wro/licenses/Application.java
@@ -3,13 +3,21 @@ package com.wro.licenses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@RefreshScope
 public class Application {
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
